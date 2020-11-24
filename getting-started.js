130522fac6aa30eba8bde5c58d90c69a65bcee56
rtanner0150 <71511920+tannertronic@users.noreply.github.com>
2020-11-23 22:08:39 -0600

    convert sql to nosql
    convert recipe tables from SQL exercise into mongoose schemas and documents

--- getting-started.js
+++ getting-started.js
@@ -6,5 +6,31 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
     //your tutorial and new code go here. 
     console.log("We're connected");
+
+    const kittySchema = new mongoose.Schema({
+        name: String
+    });
+    kittySchema.methods.speak = function(){
+        const greeting = this.name ? 'Meow name is ' + this.name : 'I don\'t have a name';
+        console.log(greeting);
+    };
+
+    const Kitten = mongoose.model('Kitten', kittySchema);
+
+    const oscar = new Kitten({name: 'Oscar'});
+    //console.log(oscar.name);
+
+    const bandit = new Kitten({name: 'Bandit'});
+    bandit.save(function(err, bandit){
+        if (err) return console.error(err);
+        bandit.speak();
+        //console.log('saved');
+    });
+
+    Kitten.find(function(err, kittens){
+        if (err) return console.error(err);
+        console.log(kittens);
+    });
+
 });
 
---
+++ recipes.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log('mongoose connected');
+
+    const recipeSchema = new mongoose.Schema({
+        name: String,
+        description: String,
+        instructions: String,
+        ingredients: [String]
+    });
+
+    const Recipe = mongoose.model('Recipe', recipeSchema);
+
+    const baconGravy = new Recipe({
+        name: 'Bacon Gravy',
+        description: 'The best gravy for biscuits',
+        instructions: 'Whisk flour into grease, add milk and continue stirring until thick, add salt to taste',
+        ingredients: ['bacon grease', 'flour', 'milk', 'salt']
+    });
+
+    const boiledEgg = new Recipe({
+        name: 'Boiled Egg',
+        description: 'A single boiled egg',
+        instructions: 'Add egg to cold water. Bring water to boil. Cook.',
+        ingredients: ['egg']
+    });
+
+    function saveRecipe(recipeObj){
+        recipeObj.save(function(err, recipeObj){
+            if (err) return console.error(err);
+            console.log('recipe saved: ' + recipeObj.name);
+        })
+    }
+
+    // saveRecipe(baconGravy);
+    // saveRecipe(boiledEgg);
+
+    // baconGravy.save(function(err, baconGravy){
+    //     if (err) return console.error(err);
+    //     console.log('recipe saved: ' + baconGravy.name);
+    // });
+    // boiledEgg.save(function(err, boiledEgg){
+    //     if (err) return console.error(err);
+    //     console.log('recipe saved: ' + boiledEgg.name);
+    // });
+
+    // baconGravy.ingredients.push('pepper');
+    // saveRecipe(baconGravy);
+
+    Recipe.find(function(err, recipes){
+        if (err) return console.error(err);
+        console.log(recipes);
+    })
+
+});
